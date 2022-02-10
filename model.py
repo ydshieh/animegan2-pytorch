f9fe6b58a4bf3b6c6986f451379bc3ca627b2181
ydshieh <ydshieh@user.noreply>
2022-02-10 08:38:50 +0100

    revert the minor change in the original model.py

--- model.py
+++ model.py
@@ -107,10 +107,4 @@ class Generator(nn.Module):
 
         out = self.out_layer(out)
         return out
-
-
-if __name__ == "__main__":
-
-    generator = Generator()
-
-    print(generator)
+        
